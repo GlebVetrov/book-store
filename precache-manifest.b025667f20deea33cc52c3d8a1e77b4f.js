60419bd59f53ba0cd905bb12b2da3cf3d79e24c3
GlebZhidovich <gzhidovich@gmail.com>
2020-07-20 22:59:48 +0300

    Updates

---
+++ precache-manifest.b025667f20deea33cc52c3d8a1e77b4f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ac49bf6f2683d5d591f4e9c8cf72178",
+    "url": "/book-store/index.html"
+  },
+  {
+    "revision": "dc28a3b37f327109bbb3",
+    "url": "/book-store/static/css/main.0a89dcf9.chunk.css"
+  },
+  {
+    "revision": "07ffe1436d8f88ca39ac",
+    "url": "/book-store/static/js/2.4fcbac59.chunk.js"
+  },
+  {
+    "revision": "dc28a3b37f327109bbb3",
+    "url": "/book-store/static/js/main.5699eb4b.chunk.js"
+  },
+  {
+    "revision": "53d1302e424e81137a5b",
+    "url": "/book-store/static/js/runtime~main.a6495b40.js"
+  }
+]);
